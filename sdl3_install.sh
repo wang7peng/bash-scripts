6eee2b07c582a44ddb25b836e3f897b129caf201
王鹏 <18795975517@163.com>
2024-07-25 02:46:42 +0800

    feat: build sdl3 with cmake

---
+++ sdl3_install.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+set -u
+
+# Desc: build sdl2+ sources
+# platform: debian 12
+# Date: 2024-7-25
+#
+# Note: 
+####################################################
+
+builddir=build-`date +%Y%m`
+prefix=/usr/local/etc/sdl
+# ---------- ---------- ---------- ---------
+
+check_tools() {
+  gcc --version 1> /dev/null
+  [ $? -eq 127 ] && exit
+
+  pkg-config --version 1> /dev/null
+  [ $? -eq 127 ] && sudo apt install -y pkg-config
+
+  pip install cmake
+}
+
+# 55M
+download_repo() {
+  local v=main
+  [ $# -eq 1 ] && v=$1
+
+  git --version 1> /dev/null
+  [ $? -eq 127 ] && sudo apt install -y git
+
+  sudo git clone --branch $v --depth 1 --single-branch \
+    https://github.com/libsdl-org/SDL.git
+}
+
+# module need
+#       alsa-lib  v1.2.8+ , in libasound2-dev
+#       jack      v1.9.21+
+#       pipewire  v0.3.65+
+#       libpulse  v0.9.15+
+#       libsndio  v1.9.0+
+#       libusb    v1.0.26+
+install_dependency() {
+  sudo apt install -y \
+    libasound2-dev libjack-jackd2-dev \
+    libpipewire-0.3-dev libpulse-dev \
+    libsndio-dev libusb-1.0-0-dev
+
+  sudo apt autoremove -y
+}
+
+# usage:
+# export PKG_CONFIG_PATH=/usr/local/etc/sdl/lib/pkgconfig
+addenv2path() {
+  if [ $(grep 'sdl/lib' ~/.bashrc | wc -l) -eq 0 ]; then
+    echo "export PKG_CONFIG_PATH=\$PKG_CONFIG_PATH:$prefix/lib/pkgconfig" | \
+      sudo tee -a ~/.bashrc
+
+    \. ~/.bashrc
+    echo "open a new terminal, run this script again!"
+    return
+  fi
+
+  # test
+  local v_pkg=PKG_CONFIG_PATH
+  eval echo '$'$v_pkg | tr ':' '\n'
+  pkg-config --cflags sdl3
+}
+
+# ---------- ---------- ---------- ---------- ----------
+cd ~/Desktop
+
+# step1 start pip environment
+python3 -m venv env4cmake
+\. ./env4cmake/bin/activate
+check_tools
+
+# step2 sources
+download_repo 
+install_dependency
+
+# step3 configure and build
+cmake -S ./SDL -B $builddir \
+    -DSDL_EXAMPLES=ON \
+    -DSDL_WAYLAND=OFF \
+    -DSDL_STATIC=ON
+
+cmake --build $builddir 
+
+[ -d $prefix ] && sudo rm -r $prefix
+sudo mkdir -p $prefix
+sudo chmod 777 -R $prefix
+
+# step4 install
+cmake --install $builddir --prefix=$prefix
+addenv2path
+
+deactivate # close python virtual env
+
